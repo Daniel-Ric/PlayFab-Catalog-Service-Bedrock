7711bd9badaa240ed7b93c9a53a326201cd7f3ad
Daniel-Ric <daanny.rickers@gmail.com>
2025-10-22 23:04:33 +0200

    PlayFab - Replaced the Enchanted Dragons ID for featured Servers

--- src/config/featuredServers.js
+++ src/config/featuredServers.js
@@ -4,7 +4,7 @@ module.exports = [
     {name: 'Lifeboat', id: '6b5e355a-be73-4f2c-8c2f-e0623517b7f6'},
     {name: 'CubeCraft', id: 'ae4d3c11-be26-4e87-b969-70e9b8c9f73a'},
     {name: 'Mineville', id: 'c9f9af01-979a-4f36-b970-70bdf2fe593e'},
-    {name: 'Enchanted Dragons', id: 'e791fe3e-b419-44fc-a121-8cfbd5fa75bd'},
+    {name: 'Enchanted Dragons', id: 'e791fe3d-b419-44fc-a121-8cfbd5fa75bd'},
     {name: 'SoulSteel', id: 'e3da296b-8be0-4d79-8608-e53d60531b7b'},
     {name: 'The Hive', id: 'f33e1256-b667-4679-b37a-91dd57fd7eba'}
 ];

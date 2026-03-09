c4060fa45443a3db74d570427613636a0a088db7
Daniel-Ric <daanny.rickers@gmail.com>
2026-03-09 08:58:33 +0100

    PlayFab - Update `searchController` to pass full query object to `service.search` and adjust formatting for consistency

--- src/controllers/marketplace/searchController.js
+++ src/controllers/marketplace/searchController.js
@@ -14,15 +14,15 @@
 
 const withETag = require("../../middleware/etag");
 const withPagination = require("../../middleware/pagination");
-const { dataCache } = require("../../config/cache");
+const {dataCache} = require("../../config/cache");
 const service = require("../../services/marketplaceService");
 const cacheKey = require("../../utils/cacheKey");
 
 exports.search = withETag(withPagination(async (req) => {
     const key = cacheKey(req);
     return dataCache.getOrSetAsync(key, async () => {
-        const { creatorName, keyword } = req.query;
-        const items = await service.search(req.params.alias, creatorName, keyword);
+        const {creatorName, keyword} = req.query;
+        const items = await service.search(req.params.alias, creatorName, keyword, req.query);
         return items;
     });
 }));
